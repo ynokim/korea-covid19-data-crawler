77a1c91205215475e0eb4ec40fe6d3715f569b3b
nulLeeKH <i_am@nulleekh.com>
2020-02-27 23:44:20 +0900

    [REFACTOR] Rename get_status function in src/status_crawler.py to get_status_data

--- src/status_crawler.py
+++ src/status_crawler.py
@@ -21,47 +21,47 @@ def dump_result(uid, data):
         json.dump(data, json_file)
 
 
-def get_status(target=''):
-    logger.info("get_status: function started | target=" + target)
+def get_status_data(target=''):
+    logger.info("get_status_data: function started | target=" + target)
 
     downloaded_html = urlopen(target)
-    logger.info("get_status: html downloaded")
+    logger.info("get_status_data: html downloaded")
     beautifulsoup_object = BeautifulSoup(downloaded_html, "html.parser")
-    logger.info("get_status: html parsed to beautifulsoup object")
+    logger.info("get_status_data: html parsed to beautifulsoup object")
 
     numbers_raw = beautifulsoup_object.findAll('a', class_='num')
-    logger.info("get_status: numbers_raw picked out")
+    logger.info("get_status_data: numbers_raw picked out")
 
     confirmed_num_str = numbers_raw[0].text
-    logger.info("get_status: confirmed_num_str extracted | dead_num_int=" + str(confirmed_num_str))
+    logger.info("get_status_data: confirmed_num_str extracted | dead_num_int=" + str(confirmed_num_str))
     confirmed_num_int = int(re.sub(',', '', confirmed_num_str[0:len(confirmed_num_str) - 2]))
-    logger.info("get_status: confirmed_num_int extracted | dead_num_int=" + str(confirmed_num_int))
+    logger.info("get_status_data: confirmed_num_int extracted | dead_num_int=" + str(confirmed_num_int))
 
     unisolated_num_str = numbers_raw[1].text
-    logger.info("get_status: unisolated_num_str extracted | dead_num_int=" + str(unisolated_num_str))
+    logger.info("get_status_data: unisolated_num_str extracted | dead_num_int=" + str(unisolated_num_str))
     unisolated_num_int = int(re.sub(',', '', unisolated_num_str[0:len(unisolated_num_str) - 2]))
-    logger.info("get_status: unisolated_num_int extracted | dead_num_int=" + str(unisolated_num_int))
+    logger.info("get_status_data: unisolated_num_int extracted | dead_num_int=" + str(unisolated_num_int))
 
     dead_num_str = numbers_raw[2].text
-    logger.info("get_status: dead_num_str extracted | dead_num_int=" + str(dead_num_str))
+    logger.info("get_status_data: dead_num_str extracted | dead_num_int=" + str(dead_num_str))
     dead_num_int = int(re.sub(',', '', dead_num_str[0:len(dead_num_str) - 2]))
-    logger.info("get_status: dead_num_int extracted | dead_num_int=" + str(dead_num_int))
+    logger.info("get_status_data: dead_num_int extracted | dead_num_int=" + str(dead_num_int))
 
     collected_result = {
         'confirmed': confirmed_num_int,
         'unisolated': unisolated_num_int,
         'dead': dead_num_int
     }
-    logger.info("get_status: collected_result generated | collected_result=" + str(collected_result))
+    logger.info("get_status_data: collected_result generated | collected_result=" + str(collected_result))
 
-    logger.info("get_status: function ended | collected_result=" + str(collected_result))
+    logger.info("get_status_data: function ended | collected_result=" + str(collected_result))
     return collected_result
 
 
 if __name__ == '__main__':
     timestamp = int(time.time()*1000)
 
-    result = get_status(target="http://ncov.mohw.go.kr/index_main.jsp")
+    result = get_status_data(target="http://ncov.mohw.go.kr/index_main.jsp")
 
     dump_result(timestamp, result)
 
